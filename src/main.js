af87ae6faf6680de17e773150e9a6dc70c551d9a
Lulzmatt27 <lulmatt27@gmail.com>
2020-11-15 18:56:45 +0300

    polyfills

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
+import "core-js";
+
 class App{
   run = async({...test}) =>{
         const name = 'Name';
         console.log(`value name: ${name}`);
-        console.log(`test: ${test}`)
+        console.log(`test: ${test}`);
+        console.log([1,2,[3,4], 5, [6,7]].flat());
 
   }
  
